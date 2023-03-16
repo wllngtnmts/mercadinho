d3d3fbc2f330a499f955e3daa4a4d3a99d022a77
wllngtnmts <60856852+wllngtnmts@users.noreply.github.com>
2023-03-16 20:00:54 -0300

    home list count product is format

--- views/admin/lista/home.php
+++ views/admin/lista/home.php
@@ -35,8 +35,13 @@
                         }else{
                             $data=$objList->getListToday();
                         }
-
+                    $quant = count($data);
+                    $totalValor = 0;
+                    $totalItens = 0;
+                    $listaValor = 0;
                     foreach($data as $List){
+                        $totalValor += $List['sis_listaValor'];
+                        $totalItens += $List['sis_listaQuant'];
                         ?>
                         <tr>
                             <td><?php echo $List['idsis_list'] ?></td>
@@ -44,14 +49,16 @@
                             <td><?php echo $List['sis_listaProduto'] ?></td>
                             <td><?php echo $List['sis_listaMarca'] ?></td>
                             <td><?php echo $List['sis_listaVolume']." ".$List['sis_listaMedida'] ?></td>
-                            <td><?php echo $List['sis_listaValor'] ?></td>
+                            <td><?php echo "R$ ".number_format($List['sis_listaValor'],2,",","."); ?></td>
                             <td><?php echo date('d-m-Y H:i:s',strtotime($List['startPruchaseProductDate'])); ?></td>
                             <td><?php echo date('d-m-Y H:i:s',strtotime($List['updatePurchaseProductDate'])); ?></td>
                             <td><?php echo $List['sis_categoriaProduto'] ?></td>
                             <td><?php echo $List['sis_listaStatus'] ?></td>
                             <td class="cadastroListEdit" data-id="<?php echo $List['idsis_list'] ?>"><img src="<?php echo DIRIMG.'edite.png' ?>" data-id="<?php echo $List['idsis_list'] ?>" alt="Editar dados"></td>
                         </tr>
-                    <?php } ?>
+                    <?php }
+                    echo $quant." linha(s), contendo ".$totalItens." iten(s) no valor de R$ ".number_format($totalValor,2,",",".");
+                    ?>
                     </tbody>
                 </table>
             </div>
